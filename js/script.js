57fdb3a1e4dcc6a0b0fc270c53812610bd969901
Oleksa <oleksa.antsupov@gmail.com>
2022-06-15 20:46:37 +0300

    add function for showing icon on user click

--- js/script.js
+++ js/script.js
@@ -3,9 +3,15 @@
 const selectBox = document.querySelector('.select-box'),
       selectXBtn = selectBox.querySelector('.playerX'),
       selectOBtn = selectBox.querySelector('.playerO'),
-      playBoard = document.querySelector('.play-board');
+      playBoard = document.querySelector('.play-board'),
+      allBox = document.querySelectorAll('section span'),
+      players = document.querySelector('.players');
 
 window.onload = () => {
+    for (let i = 0; i < allBox.length; i++) {
+        allBox[i].setAttribute('onclick', 'clickedBox(this)');
+    }
+    
     selectXBtn.addEventListener('click', () => {
         selectBox.classList.add('hide');
         playBoard.classList.add('show');
@@ -13,5 +19,20 @@ window.onload = () => {
     selectOBtn.addEventListener('click', () => {
         selectBox.classList.add('hide');
         playBoard.classList.add('show');
+        players.setAttribute('class', 'players active player');
     });
 };
+
+let playerXIcon = "fa-solid fa-xmark";
+let playerOIcon = "fa-solid fa-o";
+
+function clickedBox(element) {
+    if (players.classList.contains('player')) {
+        element.innerHTML = `<i class="${playerOIcon}"></i>`;
+        players.classList.add('active');
+    } else {
+        element.innerHTML = `<i class="${playerXIcon}"></i>`;
+        players.classList.add('active');
+    }
+    element.style.pointerEvents = "none";
+}

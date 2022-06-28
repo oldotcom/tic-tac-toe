d0741da07622f8079999d0878b9dc7f7681f9f8b
Oleksa <oleksa.antsupov@gmail.com>
2022-06-28 16:29:26 +0300

    add function for matching winning combinations

--- js/script.js
+++ js/script.js
@@ -26,10 +26,11 @@ window.onload = () => {
 let playerXIcon = "fa-solid fa-xmark";
 let playerOIcon = "fa-solid fa-o";
 let playerSign = "";
+let runBot = true;
 
 function clickedBox(element) {
     playerSign = "X";
-    
+
     if (players.classList.contains('player')) {
         element.innerHTML = `<i class="${playerOIcon}"></i>`;
         players.classList.add('active');
@@ -41,38 +42,63 @@ function clickedBox(element) {
         players.classList.add('active');
         element.setAttribute("id", playerSign);
     }
+    selectWinner();
+    
+    playBoard.style.pointerEvents = "none";
     element.style.pointerEvents = "none";
 
     let randomDelayTime = ((Math.random() * 1000) + 200).toFixed();
     
     setTimeout(() => {
-        bot();
+        bot(runBot);
     }, randomDelayTime);
 }
 
-function bot() {
-    playerSign = "O";
-    let array = [];
-    
-    for (let i = 0; i < allBox.length; i++) {
-        if (allBox[i].childElementCount == 0) {
-            array.push(i);
+function bot(runBot) {
+    if (runBot) {
+        playerSign = "O";
+        let array = [];
+        
+        for (let i = 0; i < allBox.length; i++) {
+            if (allBox[i].childElementCount == 0) {
+                array.push(i);
+            }
         }
-    }
-    let randomBox = array[Math.floor(Math.random() * array.length)];
-    
-    if (array.length > 0) {
-        if (players.classList.contains('player')) {
-            allBox[randomBox].innerHTML = `<i class="${playerXIcon}"></i>`;
-            players.classList.remove('active');
+        let randomBox = array[Math.floor(Math.random() * array.length)];
+        
+        if (array.length > 0) {
+            if (players.classList.contains('player')) {
+                allBox[randomBox].innerHTML = `<i class="${playerXIcon}"></i>`;
+                players.classList.remove('active');
 
-            playerSign = "X";
-            allBox[randomBox].setAttribute("id", playerSign);
-        } else {
-            allBox[randomBox].innerHTML = `<i class="${playerOIcon}"></i>`;
-            players.classList.remove('active');
-            allBox[randomBox].setAttribute("id", playerSign);
-        }   
+                playerSign = "X";
+                allBox[randomBox].setAttribute("id", playerSign);
+            } else {
+                allBox[randomBox].innerHTML = `<i class="${playerOIcon}"></i>`;
+                players.classList.remove('active');
+                allBox[randomBox].setAttribute("id", playerSign);
+            }
+            selectWinner();
+        }
+
+        playBoard.style.pointerEvents = "auto";
+        allBox[randomBox].style.pointerEvents = "none";
     }
-    allBox[randomBox].style.pointerEvents = "none";
+}
+
+function getID(idname) {
+    return document.querySelector(".box" + idname).id;
+}
+
+function checkID(val1, val2, val3, sign) {
+  if (getID(val1) == sign && getID(val2) == sign && getID(val3) == sign) {
+     return true;
+  } 
+}
+
+function selectWinner() {
+    if (checkID(1, 2, 3, playerSign) || checkID(4, 5, 6, playerSign) || checkID(7, 8, 9, playerSign) || (checkID(1, 4, 7, playerSign)) || checkID(2, 5, 8, playerSign) || checkID(3, 6, 9, playerSign) || checkID(1, 5, 9, playerSign) || checkID(3, 5, 7, playerSign)) {
+        runBot = false;
+        bot(runBot);
+    }
 }
